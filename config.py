8a6fbd9383d7f7e5901ee3b6af05e29bd895c588
Thomas <thomas.herfort@br.de>
2025-11-24 01:39:58 +0100

    patched backend for https

--- config.py
+++ config.py
@@ -24,7 +24,8 @@ class Settings(BaseSettings):
         "http://localhost:8081",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:8081",
-        "http://admin.mucajey.twicemind.com"
+        "http://admin.mucajey.twicemind.com",
+        "https://admin.mucajey.twicemind.com"
     ]
     
     # Paths (Docker)
